415c9436a81acd27f91d33d2ea696cebc41123f6
Mike Schutte <tmikeschutte@gmail.com>
2020-06-17 22:22:50 -0400

    Remove custom video canvas for takepicture

--- src/app/dashboard/components/item-takepicture/item-takepicture.component.ts
+++ src/app/dashboard/components/item-takepicture/item-takepicture.component.ts
@@ -1,15 +1,4 @@
-import {
-  Component,
-  OnInit,
-  Renderer2,
-  ViewChild,
-  ElementRef,
-  Input,
-  Output,
-  EventEmitter,
-  OnDestroy,
-  ChangeDetectorRef,
-} from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { Storage } from 'aws-amplify';
 import { Agreement } from '../models/agreement';
 import { UserProfile } from 'src/app/models/UserProfile';
@@ -26,28 +15,12 @@ const customPrefix = {
   templateUrl: './item-takepicture.component.html',
   styleUrls: ['./item-takepicture.component.scss'],
 })
-export class ItemTakePictureComponent implements OnInit, OnDestroy {
+export class ItemTakePictureComponent implements OnInit {
   @Input() agreement: Agreement;
   @Input() imagename: string;
   @Input() showImageArea: boolean;
 
   @Output() picturetakenEvent = new EventEmitter();
-
-  private videoElement: ElementRef;
-  @ViewChild('video', { static: false }) set content1(content: ElementRef) {
-    if (content) {
-      // initially setter gets called with undefined
-      this.videoElement = content;
-    }
-  }
-
-  private canvasElement: ElementRef;
-  @ViewChild('canvas', { static: false }) set content2(content: ElementRef) {
-    if (content) {
-      // initially setter gets called with undefined
-      this.canvasElement = content;
-    }
-  }
 
   private fileInput: HTMLInputElement;
   @ViewChild('fileInput', { static: false }) set content3(content: HTMLInputElement) {
@@ -57,31 +30,15 @@ export class ItemTakePictureComponent implements OnInit, OnDestroy {
     }
   }
 
-  videoWidth = 0;
-  videoHeight = 0;
-  constraints = {
-    video: {
-      facingMode: 'environment',
-      width: { ideal: 200 },
-      height: { ideal: 200 },
-    },
-  };
-
-  viewState: 'init' | 'capture' | 'capturePreview' | 'uploadPreview' | 'posting' = 'init';
+  viewState: 'init' | 'preview' | 'posting' = 'init';
   userProfile: UserProfile;
   imgSrc = '';
 
-  constructor(private renderer: Renderer2, private userService: UserService, private cd: ChangeDetectorRef) {}
+  constructor(private userService: UserService, private cd: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.userProfile = this.userService.getCurrentUserProfile();
     customPrefix.public = 'public/' + this.userProfile.id + '/' + this.agreement.shareId + '/';
-    this.startCamera();
-    this.viewState = 'capture';
-  }
-
-  ngOnDestroy() {
-    this.stopCamera();
   }
 
   get showUpload() {
@@ -92,15 +49,23 @@ export class ItemTakePictureComponent implements OnInit, OnDestroy {
     return true;
   }
 
+  get showPreview() {
+    switch (this.viewState) {
+      case 'preview':
+        return true;
+    }
+    return false;
+  }
+
   loadpicture({ target }: Event & { target: HTMLInputElement }) {
     this.imgSrc = '';
     const file = target.files[0];
-    this.viewState = 'uploadPreview';
+    this.viewState = 'preview';
 
     const reader = new FileReader();
     reader.onload = e => {
       this.imgSrc = (<FileReader>e.target).result as string;
-      this.viewState = 'uploadPreview';
+      this.viewState = 'preview';
       this.cd.detectChanges();
     };
     reader.readAsDataURL(file); // convert to base64 string
@@ -117,45 +82,14 @@ export class ItemTakePictureComponent implements OnInit, OnDestroy {
     this.fileInput.value = '';
   }
 
-  get showCapture() {
-    switch (this.viewState) {
-      case 'init':
-      case 'capture':
-        return this.canUseCamera;
-    }
-    return false;
-  }
-  captureimage() {
-    this.renderer.setProperty(this.canvasElement.nativeElement, 'width', this.videoWidth);
-    this.renderer.setProperty(this.canvasElement.nativeElement, 'height', this.videoHeight);
-    this.canvasElement.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
-    this.imgSrc = this.canvasElement.nativeElement.toDataURL('image/svg');
-    this.viewState = 'capturePreview';
-    console.log('agreement', this.agreement);
-  }
-
-  get showRetake() {
-    switch (this.viewState) {
-      case 'uploadPreview':
-      case 'capturePreview':
-        return this.canUseCamera;
-    }
-    return false;
-  }
-  retakepicture() {
-    this.viewState = 'capture';
-    this.imgSrc = '';
-    this.startCamera();
-  }
-
   get showAccept() {
     switch (this.viewState) {
-      case 'capturePreview':
-      case 'uploadPreview':
+      case 'preview':
         return true;
     }
     return false;
   }
+
   acceptimage() {
     this.viewState = 'posting';
     console.log(`the image (${this.imagename}):`, this.imgSrc);
@@ -172,47 +106,10 @@ export class ItemTakePictureComponent implements OnInit, OnDestroy {
         this.showImageArea = false;
         this.viewState = 'init';
         this.picturetakenEvent.emit(this.showImageArea);
-        this.stopCamera();
       })
       .catch(err => {
         console.log('error =>', err);
-        this.viewState = 'capture';
+        this.viewState = 'init';
       });
-  }
-
-  get canUseCamera() {
-    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-  }
-  startCamera() {
-    if (this.canUseCamera) {
-      navigator.mediaDevices
-        .getUserMedia(this.constraints)
-        .then(this.attachVideo.bind(this))
-        .catch(this.handleError);
-    } else {
-      alert('Sorry, camera not available.');
-    }
-  }
-
-  attachVideo(stream) {
-    this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
-    this.renderer.listen(this.videoElement.nativeElement, 'play', event => {
-      this.videoHeight = this.videoElement.nativeElement.videoHeight;
-      this.videoWidth = this.videoElement.nativeElement.videoWidth;
-    });
-  }
-
-  handleError(error) {
-    console.log('Error: ', error);
-  }
-
-  stopCamera() {
-    const videoElem = this.videoElement.nativeElement.srcObject;
-    const tracks = videoElem.getTracks();
-    tracks.forEach(function(track) {
-      track.stop();
-    });
-
-    videoElem.srcObject = '';
   }
 }
